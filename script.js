4e6404f051339f476bf37e124408558cdcd38f21
kaxkata <gabrielr387@gmail.com>
2019-08-17 12:21:56 -0300

    modificação js

---
+++ script.js
@@ -0,0 +1,12 @@
+function verifyEmail(){
+     var checkemail  = formulario.email.value;
+     
+
+    if (email == "" || email.indexOf('@' && ".") == -1 ){
+         alert("Email inválido");
+         formulario.nome;
+         return false;
+     }
+
+
+}
